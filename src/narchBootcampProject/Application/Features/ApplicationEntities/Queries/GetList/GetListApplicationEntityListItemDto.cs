f620aefe7f078ade557317e10ee0d42caf7dab59
Burak Yazıcı <133709862+YaziciBurak@users.noreply.github.com>
2024-05-07 13:31:16 +0300

    update:Application

--- src/narchBootcampProject/Application/Features/ApplicationEntities/Queries/GetList/GetListApplicationEntityListItemDto.cs
+++ src/narchBootcampProject/Application/Features/ApplicationEntities/Queries/GetList/GetListApplicationEntityListItemDto.cs
@@ -6,6 +6,10 @@ public class GetListApplicationEntityListItemDto : IDto
 {
     public int Id { get; set; }
     public Guid ApplicantId { get; set; }
+    public string ApplicantFirstName { get; set; }
+    public string ApplicantLastName { get; set; }
     public int BootcampId { get; set; }
+    public string BootcampName { get; set; }
     public int ApplicationStateId { get; set; }
+    public string ApplicationStateName { get; set; }
 }
