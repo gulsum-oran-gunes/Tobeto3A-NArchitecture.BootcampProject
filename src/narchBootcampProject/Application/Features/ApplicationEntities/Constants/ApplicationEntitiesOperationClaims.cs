dc7aa05c5912c15551847aa39b55cd3785fe54c7
GÜLSÜM ORAN GÜNEŞ <avgulsumoran@gmail.com>
2024-05-30 21:29:43 +0300

    student operation claim bug fixed

--- src/narchBootcampProject/Application/Features/ApplicationEntities/Constants/ApplicationEntitiesOperationClaims.cs
+++ src/narchBootcampProject/Application/Features/ApplicationEntities/Constants/ApplicationEntitiesOperationClaims.cs
@@ -15,5 +15,5 @@ public static class ApplicationEntitiesOperationClaims
     public const string Create = $"{_section}.Create";
     public const string Update = $"{_section}.Update";
     public const string Delete = $"{_section}.Delete";
-    public const string Student = $"{_section}.Student";
+    public const string Student = "Student";
 }
--- src/narchBootcampProject/Application/Features/Results/Constants/ResultsOperationClaims.cs
+++ src/narchBootcampProject/Application/Features/Results/Constants/ResultsOperationClaims.cs
@@ -15,5 +15,5 @@ public static class ResultsOperationClaims
     public const string Create = $"{_section}.Create";
     public const string Update = $"{_section}.Update";
     public const string Delete = $"{_section}.Delete";
-    public const string Student = $"{_section}.Student";
+    public const string Student = "Student";
 }
